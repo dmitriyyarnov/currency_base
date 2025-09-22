b4130080ea03e1c5977e2fb705ba29c5fa4b3cc0
dmitriyyarnov <78679511+dmitriyyarnov@users.noreply.github.com>
2025-09-23 00:41:36 +0300

    del

--- flow/usd_flow.py
+++ flow/usd_flow.py
@@ -1,3 +1,5 @@
+import schedule
+import time
 from prefect import flow, task
 from scraper.scraper import run_scraper
 
@@ -9,5 +11,18 @@ def fetch_and_save():
 def usd_scraper_flow():
     fetch_and_save()
 
-if __name__ == "__main__":
+
+def run_flow():
     usd_scraper_flow()
+
+# Запуск каждый день в час:мин
+schedule.every().day.at("00:35").do(run_flow)
+
+print("Scheduler started...")
+
+# Цикл, чтобы проверять задачи каждую минуту
+while True:
+    schedule.run_pending()
+    time.sleep(60)
+
+
